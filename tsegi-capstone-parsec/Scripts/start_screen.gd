45263413bb1b333852f5f33f7c4ae954bac0ff06
takumisegi <takumi.segi@gmail.com>
2025-05-06 02:34:06 -0400

    Main Menu - Fixed

--- tsegi-capstone-parsec/Scripts/start_screen.gd
+++ tsegi-capstone-parsec/Scripts/start_screen.gd
@@ -6,3 +6,14 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 	
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+func _on_story_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Story.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
